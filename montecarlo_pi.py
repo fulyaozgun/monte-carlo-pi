e40900822565075ddabf7372ab8c6322c4734eab
fulyaozgun <fulyazgn@gmail.com>
2026-05-31 20:51:11 +0300

    Update montecarlo_pi.py

--- montecarlo_pi.py
+++ montecarlo_pi.py
@@ -1,7 +1,15 @@
 import random
+import matplotlib.pyplot as plt
+
+inside_x = []
+inside_y = []
+
+outside_x = []
+outside_y = []
+
+total_points = 5000
 
 inside_circle = 0
-total_points = 100000
 
 for i in range(total_points):
     x = random.uniform(-1, 1)
@@ -9,7 +17,20 @@ for i in range(total_points):
 
     if x**2 + y**2 <= 1:
         inside_circle += 1
+        inside_x.append(x)
+        inside_y.append(y)
+    else:
+        outside_x.append(x)
+        outside_y.append(y)
 
 pi_estimate = 4 * inside_circle / total_points
 
 print("Estimated Pi:", pi_estimate)
+
+plt.scatter(inside_x, inside_y, s=1)
+plt.scatter(outside_x, outside_y, s=1)
+
+plt.title(f"Monte Carlo Pi Estimate = {pi_estimate}")
+plt.axis("equal")
+
+plt.show()
